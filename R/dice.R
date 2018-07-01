599e550618e2c791c2e0521c6bfcc605994cfe41
Tyelcie <tyelcie@gmail.com>
2018-07-01 23:03:54 +0800

    test

--- R/dice.R
+++ R/dice.R
@@ -1,6 +1,11 @@
 #' play a dice-tossing game
 #'
-#' @param any options you're considering
+#' @description When you don't know what to do, this helps you to make a
+#' final decision via a simulated coin tossing or dice tossing game.
+#' @param Points a character vector specifying all the options you're
+#' wagging amongst.
+#' @examples Points <- c('Do the Laundry','Homework','Cook','Go to Bed')
+#'
 dice <- function(Points){
   Num <- sample(1:length(Points),1)
   plot.new()
--- R/trans.R
+++ R/trans.R
@@ -3,6 +3,9 @@
 #' @param row.names logic to define if the new rowname should be of the original colnames
 #' @param col.names similar to the row.names
 #' @import data.table
+#' @details Transpose a data frame probably with dimnames. If there are
+#' column names or row names, you can determine wether to have them also
+#' transposed.
 
 trans <- function(x,row.names = T,col.names = T){
   y <- transpose(x)
