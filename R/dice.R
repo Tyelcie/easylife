c81135f4419a05f2981ba13c5e367e39db114362
Tyelcie <tyelcie@gmail.com>
2018-09-26 10:25:40 +0800

    colleting argument

--- R/dice.R
+++ R/dice.R
@@ -2,14 +2,16 @@
 #'
 #' @description When you don't know what to do, this helps you to make a
 #' final decision via a simulated coin tossing or dice tossing game.
-#' @param ... a character vector specifying all the options you're
-#' wagging amongst.
-#' @examples Points <- c('Do the Laundry','Homework','Cook','Go to Bed')
-#' @return Whatever it returns, gonna do it! Or you might think that when a
-#' coin trows in the air you finally know your true hope? HEHE
+#' @param ... please specify all the options you're wagging amongst.
+#' @examples dice('Do the Laundry','Homework','Cook','Go to Bed')
+#' @return Whatever it returns, gonna do it! It is said that the true point
+#' of tossing is that, when waiting for the coin or dice in the air you finally
+#' know your hope. hehe
 #'
 #' @export
 dice <- function(...){
+  message('tossing...')
+  Sys.sleep(3)
   Rslt <- sample(c(...),1)
   par(family='STHeitiSC-Medium')
   plot.new()
