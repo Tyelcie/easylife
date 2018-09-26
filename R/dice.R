9bbf56842e2b9d06d657ed9da6db8cab2c310d52
Tyelcie <tyelcie@gmail.com>
2018-09-26 10:18:30 +0800

    collecting parameters

--- R/dice.R
+++ R/dice.R
@@ -2,16 +2,17 @@
 #'
 #' @description When you don't know what to do, this helps you to make a
 #' final decision via a simulated coin tossing or dice tossing game.
-#' @param Points a character vector specifying all the options you're
+#' @param ... a character vector specifying all the options you're
 #' wagging amongst.
 #' @examples Points <- c('Do the Laundry','Homework','Cook','Go to Bed')
 #' @return Whatever it returns, gonna do it! Or you might think that when a
 #' coin trows in the air you finally know your true hope? HEHE
 #'
 #' @export
-dice <- function(Points){
-  Num <- sample(1:length(Points),1)
+dice <- function(...){
+  Rslt <- sample(c(...),1)
+  par(family='STHeitiSC-Medium')
   plot.new()
-  text(0.5,0.5,Points[Num],cex = 4)
-  return(Points[Num])
+  text(0.5,0.5,Rslt,cex = 4)
+  return(Rslt)
 }
